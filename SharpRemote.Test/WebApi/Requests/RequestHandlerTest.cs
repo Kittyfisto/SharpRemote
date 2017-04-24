e96bdbacefd139b52f30b741e530547fc73b3371
Kittyfisto <kittyfisto87@gmail.com>
2017-04-24 23:39:36 +0200

    Added more tests

--- SharpRemote.Test/WebApi/Requests/RequestHandlerTest.cs
+++ SharpRemote.Test/WebApi/Requests/RequestHandlerTest.cs
@@ -42,5 +42,18 @@ namespace SharpRemote.Test.WebApi.Requests
 			Encoding.UTF8.GetString(response.Content).Should().Be("[\"Hello\",\"World!\"]");
 			resource.Verify(x => x.Get(), Times.Once);
 		}
+
+		[Test]
+		public void TestGetStringList2()
+		{
+			var resource = new Mock<IGetStringList>();
+			resource.Setup(x => x.Get(It.Is<int>(y => y == 9001))).Returns("Sup");
+			var handler = Resource.Create(resource.Object);
+			var response = handler.TryHandleRequest("9001", new WebRequest(Empty, HttpMethod.Get));
+			response.Should().NotBeNull();
+			response.Code.Should().Be(200);
+			response.Encoding.Should().Be(Encoding.UTF8);
+			Encoding.UTF8.GetString(response.Content).Should().Be("\"Sup\"");
+		}
 	}
 }

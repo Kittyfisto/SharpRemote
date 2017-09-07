59485e23ca63d8cf538b9e3a6343e5cc4b1e8b58
Kittyfisto <kittyfisto87@gmail.com>
2017-09-07 21:04:22 +0200

    Added test for Message parsing

---
+++ SharpRemote.Test/ServiceDiscovery/MessageTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using SharpRemote.ServiceDiscovery;
+using System.Collections.Generic;
+
+namespace SharpRemote.Test.ServiceDiscovery
+{
+	[TestFixture]
+	public sealed class MessageTest
+	{
+		public static IEnumerable<byte[]> InvalidMessages => new[]
+		{
+			new byte[0],
+			new byte[3],
+			new byte[10]
+
+			// Doesn't work (yet)
+			//new byte[1] {1}
+		};
+
+		[Test]
+		[Description("Verifies that TryRead doesn't throw when the stream is too small or contains gibberish data")]
+		public void TestTryRead1([ValueSource(nameof(InvalidMessages))] byte[] message)
+		{
+			string token;
+			string name;
+			IPEndPoint endPoint;
+			Message.TryRead(message, out token, out name, out endPoint).Should().BeFalse();
+		}
+	}
+}

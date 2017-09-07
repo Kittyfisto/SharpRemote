761c5cd5893b67ab13106463839d15957a0ccb1b
Kittyfisto <kittyfisto87@gmail.com>
2017-09-07 22:56:54 +0200

    Removed unused project

--- SharpRemote.Windows/Hosting/HostState.cs
+++ SharpRemote.Windows/Hosting/HostState.cs
@@ -1,16 +1,38 @@
 ﻿namespace SharpRemote.Hosting
 {
 	/// <summary>
-	/// Defines the various states of the host process.
+	///     Defines the various states of the host process.
 	/// </summary>
 	public enum HostState
 	{
+		/// <summary>
+		///     Default state, should not appear in the wild.
+		/// </summary>
 		None,
 
+		/// <summary>
+		///     The host will boot in the near future.
+		/// </summary>
 		BootPending,
+
+		/// <summary>
+		///     The host is booting up, but hasn't finished yet.
+		/// </summary>
 		Booting,
+
+		/// <summary>
+		///     The host has booted up and is ready to be used.
+		/// </summary>
 		Ready,
+
+		/// <summary>
+		///     The host is shutting down.
+		/// </summary>
 		ShuttingDown,
-		Dead,
+
+		/// <summary>
+		///     The host process is no longer running.
+		/// </summary>
+		Dead
 	}
 }

6976f2be96b3e21fe70213d0d9c961b213616a4e
Kittyfisto <kittyfisto87@gmail.com>
2017-11-07 01:14:38 +0100

    Added more documentation

--- SharpRemote.Windows/CodeGeneration/Serialization/ISerializer2.cs
+++ SharpRemote.Windows/CodeGeneration/Serialization/ISerializer2.cs
@@ -26,8 +26,16 @@ namespace SharpRemote
 	{
 		/// <summary>
 		///     Creates a new writer which writes the intention to call the given method on the given object
-		///     to the given stream. The stream should be fully written to once the given writer is disposed of.
+		///     to the given stream.
 		/// </summary>
+		/// <remarks>
+		///     It is expected that <see cref="CreateMethodInvocationReader" /> can read the contents of the stream
+		///     written to by this method.
+		/// </remarks>
+		/// <remarks>
+		///     The given <paramref name="stream" /> should NOT be written to any more after
+		///     the returned writer has been disposed of.
+		/// </remarks>
 		/// <param name="stream"></param>
 		/// <param name="grainId"></param>
 		/// <param name="methodName"></param>
@@ -37,19 +45,44 @@ namespace SharpRemote
 
 		/// <summary>
 		/// </summary>
+		/// <remarks>
+		///     The given <paramref name="stream" /> should NOT be read from any more after
+		///     the returned reader has been disposed of.
+		/// </remarks>
+		/// <remarks>
+		///     It is expected that this method can read a stream written to by <see cref="CreateMethodInvocationWriter" />.
+		/// </remarks>
 		/// <param name="stream">The stream from which the details of the method invocation are read from</param>
 		/// <returns></returns>
 		IMethodInvocationReader CreateMethodInvocationReader(Stream stream);
 
 		/// <summary>
+		///     Creates a writer which writes the result (or exception) foa method invocation to the given
+		///     <paramref name="stream" />.
 		/// </summary>
+		/// <remarks>
+		///     It is expected that <see cref="CreateMethodResultReader" /> can read the contents of the stream
+		///     written to by this method.
+		/// </remarks>
+		/// <remarks>
+		///     The given <paramref name="stream" /> should NOT be written to any more after
+		///     the returned writer has been disposed of.
+		/// </remarks>
 		/// <param name="stream">The stream to which the result (or exception) of the method invocation are written to</param>
 		/// <param name="rpcId"></param>
 		/// <returns></returns>
 		IMethodResultWriter CreateMethodResultWriter(Stream stream, ulong rpcId);
 
 		/// <summary>
+		///     Creates a reader which consumes the given stream and reads the result of a method invocation from it.
 		/// </summary>
+		/// <remarks>
+		///     It is expected that this method can read a stream written to by <see cref="CreateMethodResultWriter" />.
+		/// </remarks>
+		/// <remarks>
+		///     The given <paramref name="stream" /> should NOT be read from any more after
+		///     the returned reader has been disposed of.
+		/// </remarks>
 		/// <param name="stream">The stream from which the result of the method invocation is read from</param>
 		/// <returns></returns>
 		IMethodResultReader CreateMethodResultReader(Stream stream);

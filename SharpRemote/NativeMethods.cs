3da391d9be749aa58b4f871d5432be8ed2f182ef
Kittyfisto <kittyfisto87@gmail.com>
2018-03-08 22:10:48 +0100

    NativeMethods can now be accessed publicly #46

--- SharpRemote/NativeMethods.cs
+++ SharpRemote/NativeMethods.cs
@@ -2,29 +2,49 @@
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using log4net;
 using SharpRemote.Extensions;
 using SharpRemote.Hosting;
-using log4net;
 
 namespace SharpRemote
 {
-	internal static class NativeMethods
+	/// <summary>
+	///     Provides access to native methods from "SharpRemote.PostmortemDebugger.dll":
+	///     Allows interception of various failures (such as access violations, pure virtual
+	///     function calls, assertions, etc..) and minidump creation when they occur.
+	/// </summary>
+	public static class NativeMethods
 	{
-		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
+		/// <summary>
+		/// </summary>
 		[Flags]
 		public enum ErrorModes : uint
 		{
 // ReSharper disable InconsistentNaming
+			/// <summary>
+			/// </summary>
 			SYSTEM_DEFAULT = 0x0,
+
+			/// <summary>
+			/// </summary>
 			SEM_FAILCRITICALERRORS = 0x0001,
+
+			/// <summary>
+			/// </summary>
 			SEM_NOALIGNMENTFAULTEXCEPT = 0x0004,
+
+			/// <summary>
+			/// </summary>
 			SEM_NOGPFAULTERRORBOX = 0x0002,
+
+			/// <summary>
+			/// </summary>
 			SEM_NOOPENFILEERRORBOX = 0x8000
 // ReSharper restore InconsistentNaming
 		}
 
 		private const string PostmortdemDebuggerDll = "SharpRemote.PostmortemDebugger.dll";
+		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 		private static readonly object SyncRoot = new object();
 		private static IntPtr _library;
 
@@ -46,14 +66,14 @@ namespace SharpRemote
 				library = LoadLibrary(libraryPath);
 				if (library == IntPtr.Zero)
 				{
-					string directory = Assembly.GetExecutingAssembly().GetDirectory();
+					var directory = Assembly.GetExecutingAssembly().GetDirectory();
 					if (directory != null)
 					{
-						string path = Environment.GetEnvironmentVariable("PATH");
+						var path = Environment.GetEnvironmentVariable("PATH");
 						path = string.Format("{0};{1}",
 						                     Path.Combine(directory, Environment.Is64BitProcess ? "x64" : "x86"),
 						                     path
-							);
+						                    );
 						Environment.SetEnvironmentVariable("PATH", path);
 
 						library = LoadLibrary(libraryPath);
@@ -70,21 +90,53 @@ namespace SharpRemote
 		[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
 		private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
 
+		/// <summary>
+		///     Adds a directory to the search path used to locate DLLs for the application.
+		///     <see
+		///         href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms686203%28v=vs.85%29.aspx" />
+		/// </summary>
+		/// <param name="pathName"></param>
+		/// <returns></returns>
 		[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		public static extern bool SetDllDirectory(string pathName);
 
+		/// <summary>
+		///     Opens an existing local process object.
+		///     <see
+		///         href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms684320%28v=vs.85%29.aspx" />
+		///     .
+		/// </summary>
+		/// <param name="processAccess"></param>
+		/// <param name="bInheritHandle"></param>
+		/// <param name="processId"></param>
+		/// <returns></returns>
 		[DllImport("kernel32.dll", SetLastError = true)]
 		public static extern IntPtr OpenProcess(
-			 ProcessAccessFlags processAccess,
-			 bool bInheritHandle,
-			 int processId
+			ProcessAccessFlags processAccess,
+			bool bInheritHandle,
+			int processId
 		);
 
+		/// <summary>
+		///     Terminates the specified process and all of its threads.
+		///     <see
+		///         href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms686714%28v=vs.85%29.aspx" />
+		/// </summary>
+		/// <param name="hProcess"></param>
+		/// <param name="uExitCode"></param>
+		/// <returns></returns>
 		[DllImport("kernel32.dll", SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		public static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
 
+		/// <summary>
+		///     Closes an open object handle.
+		///     <see
+		///         href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211%28v=vs.85%29.aspx" />
+		/// </summary>
+		/// <param name="hObject"></param>
+		/// <returns></returns>
 		[DllImport("kernel32.dll", SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		public static extern bool CloseHandle(IntPtr hObject);
@@ -101,19 +153,29 @@ namespace SharpRemote
 			int numRetainedMinidumps,
 			string dumpFolder,
 			string dumpName
-			);
+		);
 
+		/// <summary>
+		///     Configures the post mortem debugger of the current process
+		///     to store minidumps in the given folder.
+		///     If there are more than the given amount of minidumps, then the oldest
+		///     minidumps are removed upon creation of new dumps.
+		/// </summary>
+		/// <param name="numRetainedMinidumps"></param>
+		/// <param name="dumpFolder"></param>
+		/// <param name="dumpName"></param>
+		/// <returns></returns>
 		public static bool InitDumpCollection(
 			int numRetainedMinidumps,
 			string dumpFolder,
 			string dumpName
-			)
+		)
 		{
 			try
 			{
 				if (!_initDumpCollection(numRetainedMinidumps, dumpFolder, dumpName))
 				{
-					int err = Marshal.GetLastWin32Error();
+					var err = Marshal.GetLastWin32Error();
 					Log.ErrorFormat("Unable to initialize the post-mortem debugger: {0}", err);
 					return false;
 				}
@@ -133,11 +195,21 @@ namespace SharpRemote
 			EntryPoint = "InstallPostmortemDebugger")]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		private static extern bool _installPostmortemDebugger(bool suppressErrorWindows,
-		                                                    bool interceptUnhandledExceptions,
-		                                                    bool handleCrtAsserts,
-		                                                    bool handleCrtPurecalls,
-		                                                    CRuntimeVersions crtVersions);
+		                                                      bool interceptUnhandledExceptions,
+		                                                      bool handleCrtAsserts,
+		                                                      bool handleCrtPurecalls,
+		                                                      CRuntimeVersions crtVersions);
 
+		/// <summary>
+		///     Installs a post mortem debugger in the current process which handles the given failures.
+		///     Should a failure occur, a minidump is written automatically.
+		/// </summary>
+		/// <param name="suppressErrorWindows"></param>
+		/// <param name="interceptUnhandledExceptions"></param>
+		/// <param name="handleCrtAsserts"></param>
+		/// <param name="handleCrtPurecalls"></param>
+		/// <param name="crtVersions"></param>
+		/// <returns></returns>
 		public static bool InstallPostmortemDebugger(bool suppressErrorWindows,
 		                                             bool interceptUnhandledExceptions,
 		                                             bool handleCrtAsserts,
@@ -152,9 +224,9 @@ namespace SharpRemote
 				                                handleCrtPurecalls,
 				                                crtVersions))
 				{
-					int err = Marshal.GetLastWin32Error();
+					var err = Marshal.GetLastWin32Error();
 					Log.ErrorFormat("Unable to install the post-mortem debugger for unhandled exceptions: {0}",
-									err);
+					                err);
 					return false;
 				}
 
@@ -163,18 +235,24 @@ namespace SharpRemote
 			catch (Exception e)
 			{
 				Log.ErrorFormat("Unable to install the post-mortem debugger for unhandled exceptions: {0}",
-									e);
+				                e);
 				return false;
 			}
 		}
 
+		/// <summary>
+		///     Creates a minidump of the given process and stores it in the given file.
+		/// </summary>
+		/// <param name="processId"></param>
+		/// <param name="dumpName"></param>
+		/// <returns></returns>
 		[DllImport(PostmortdemDebuggerDll, SetLastError = true, CharSet = CharSet.Unicode,
 			CallingConvention = CallingConvention.Cdecl)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		public static extern bool CreateMiniDump(
 			int processId,
 			string dumpName
-			);
+		);
 
 		#endregion
 	}
--- SharpRemote/ProcessAccessFlags.cs
+++ SharpRemote/ProcessAccessFlags.cs
@@ -2,21 +2,75 @@
 
 namespace SharpRemote
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	[Flags]
-	internal enum ProcessAccessFlags : uint
+	public enum ProcessAccessFlags : uint
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		All = 0x001F0FFF,
+
+		/// <summary>
+		/// Required to terminate a process using TerminateProcess.
+		/// </summary>
 		Terminate = 0x00000001,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		CreateThread = 0x00000002,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		VirtualMemoryOperation = 0x00000008,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		VirtualMemoryRead = 0x00000010,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		VirtualMemoryWrite = 0x00000020,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		DuplicateHandle = 0x00000040,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		CreateProcess = 0x000000080,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		SetQuota = 0x00000100,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		SetInformation = 0x00000200,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		QueryInformation = 0x00000400,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		QueryLimitedInformation = 0x00001000,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Synchronize = 0x00100000
 	}
 }
